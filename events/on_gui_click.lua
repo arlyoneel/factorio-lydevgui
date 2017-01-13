7b26a047df18d2b7f87ad6043f3a7925761a7190
Federico Leonel La Porta <federico.leonel@protonmail.com>
2017-01-12 23:23:12 -0300

    * changed the way that i handle double clicks on "..." button, i hope this fix -> https://github.com/arlyoneel/factorio-lydevgui/issues/10

--- events/on_gui_click.lua
+++ events/on_gui_click.lua
@@ -1,78 +1,67 @@
 script.on_event(defines.events.on_gui_click, function(event)
-    local evtName
 
-    -- workaround for fake events, event object is read only
-    if (global.fakeEvent) then
-        evtName = global.fakeEvent
-        global.fakeEvent = false;
-    else
-        evtName = event.element.name
-    end
+    if (event.element.name == "showOptions" and
+        LyDevGUI.gui.optsRoot.frame == nil) then
 
-    if (evtName == "showOptions") then
-        if(LyDevGUI.gui.optsRoot.frame == nil) then
+        LyDevGUI.gui.optsRoot.add{ type="frame", name="frame", caption="", direction="vertical" }
+        LyDevGUI.gui.optsRoot.frame.add{ type="table", name="frameTitle", colspan=3 }
 
-            LyDevGUI.gui.optsRoot.add{ type="frame", name="frame", caption="", direction="vertical" }
-            LyDevGUI.gui.optsRoot.frame.add{ type="table", name="frameTitle", colspan=3 }
+        -- hack to use X to close
+        LyDevGUI.gui.optsRoot.frame.frameTitle.add{
+            type="label", name="frameTitle", caption={"txt.mod.options.frameTitle", CONST.INFO.MOD_NAME .. " v" ..  CONST.INFO.MOD_VERSION}
+        }
+        LyDevGUI.gui.optsRoot.frame.frameTitle.add{
+            type="label", name="frameSeparator", caption="                 "
+        }
+        LyDevGUI.gui.optsRoot.frame.frameTitle.add{
+            type="button", name="frameTitleClose", caption=" X "
+        }
 
-            -- hack to use X to close
-            LyDevGUI.gui.optsRoot.frame.frameTitle.add{
-                type="label", name="frameTitle", caption={"txt.mod.options.frameTitle", CONST.INFO.MOD_NAME .. " v" ..  CONST.INFO.MOD_VERSION}
-            }
-            LyDevGUI.gui.optsRoot.frame.frameTitle.add{
-                type="label", name="frameSeparator", caption="                 "
-            }
-            LyDevGUI.gui.optsRoot.frame.frameTitle.add{
-                type="button", name="frameTitleClose", caption=" X "
-            }
+        -- option misc label
+        LyDevGUI.gui.optsRoot.frame.add{ type="label",
+            name="generalLabel", caption={"txt.mod.options.generalLabel" }
+        }
+        -- option misc
+        LyDevGUI.gui.optsRoot.frame.add{ type="checkbox",
+            name="showVarsWOValues", state=LyDevGUI.options.showVarsWOValues,
+            caption={"txt.mod.options.showVarsWOValues"}
+        }
 
-            -- option misc label
-            LyDevGUI.gui.optsRoot.frame.add{ type="label",
-                name="generalLabel", caption={"txt.mod.options.generalLabel" }
-            }
-            -- option misc
-            LyDevGUI.gui.optsRoot.frame.add{ type="checkbox",
-                name="showVarsWOValues", state=LyDevGUI.options.showVarsWOValues,
-                caption={"txt.mod.options.showVarsWOValues"}
-            }
+        -- option vars label
+        LyDevGUI.gui.optsRoot.frame.add{ type="label", name="contentLabel",
+            caption={"txt.mod.options.varsLabel" }
+        }
+        -- option vars table
+        LyDevGUI.gui.optsRoot.frame.add{ type="table", name="varTbl", colspan=2 }
+        -- options vars
+        LyDevGUI.gui.optsRoot.frame.varTbl.add{
+            type="checkbox", name="showPlayerVars",
+            state=LyDevGUI.options.showPlayerVars,
+            caption={"txt.show1", "player"}
+        }
 
-            -- option vars label
-            LyDevGUI.gui.optsRoot.frame.add{ type="label", name="contentLabel",
-                caption={"txt.mod.options.varsLabel" }
-            }
-            -- option vars table
-            LyDevGUI.gui.optsRoot.frame.add{ type="table", name="varTbl", colspan=2 }
-            -- options vars
-            LyDevGUI.gui.optsRoot.frame.varTbl.add{
-                type="checkbox", name="showPlayerVars",
-                state=LyDevGUI.options.showPlayerVars,
-                caption={"txt.show1", "player"}
-            }
+        LyDevGUI.gui.optsRoot.frame.varTbl.add{
+            type="checkbox", name="showSelectedVars",
+            state=LyDevGUI.options.showSelectedVars,
+            caption={"txt.show1", "selected"}
+        }
 
-            LyDevGUI.gui.optsRoot.frame.varTbl.add{
-                type="checkbox", name="showSelectedVars",
-                state=LyDevGUI.options.showSelectedVars,
-                caption={"txt.show1", "selected"}
-            }
+        LyDevGUI.gui.optsRoot.frame.varTbl.add{
+            type="checkbox", name="showStackVars",
+            state=LyDevGUI.options.showStackVars,
+            caption={"txt.show1", "stack"}
+        }
 
-            LyDevGUI.gui.optsRoot.frame.varTbl.add{
-                type="checkbox", name="showStackVars",
-                state=LyDevGUI.options.showStackVars,
-                caption={"txt.show1", "stack"}
-            }
+        LyDevGUI.gui.optsRoot.frame.varTbl.add{
+            type="checkbox", name="showProtoVars",
+            state=LyDevGUI.options.showProtoVars,
+            caption={"txt.show1", "prototype"}
+        }
 
-            LyDevGUI.gui.optsRoot.frame.varTbl.add{
-                type="checkbox", name="showProtoVars",
-                state=LyDevGUI.options.showProtoVars,
-                caption={"txt.show1", "prototype"}
-            }
-        else
-            -- call event with "frameTitleClose"
-            -- fake event, because original event is read only
-            global.fakeEvent = "frameTitleClose"
-            game.raise_event(events.on_gui_click, { event = {}, })
-        end
-    elseif (evtName == "frameTitleClose") then
+    elseif (event.element.name == "frameTitleClose" or
+        (event.element.name == "showOptions" and
+        LyDevGUI.gui.optsRoot.frame ~= nil) ) then
+
         if(LyDevGUI.gui.optsRoot.frame ~= nil) then
             LyDevGUI.gui.optsRoot.frame.destroy()
             -- refresh labels
@@ -80,31 +69,31 @@ script.on_event(defines.events.on_gui_click, function(event)
                 player = Ly.getPlayer(),
             })
         end
-    elseif (evtName == "showPlayerVars") then
+    elseif (event.element.name == "showPlayerVars") then
         if (event.element.state == true) then
             LyDevGUI.options.showPlayerVars = true;
         else
             LyDevGUI.options.showPlayerVars = false;
         end
-    elseif (evtName == "showStackVars") then
+    elseif (event.element.name == "showStackVars") then
         if (event.element.state == true) then
             LyDevGUI.options.showStackVars = true;
         else
             LyDevGUI.options.showStackVars = false;
         end
-    elseif (evtName == "showProtoVars") then
+    elseif (event.element.name == "showProtoVars") then
         if (event.element.state == true) then
             LyDevGUI.options.showProtoVars = true;
         else
             LyDevGUI.options.showProtoVars = false;
         end
-    elseif( evtName == "showSelectedVars") then
+    elseif( event.element.name == "showSelectedVars") then
         if (event.element.state == true) then
             LyDevGUI.options.showSelectedVars = true;
         else
             LyDevGUI.options.showSelectedVars = false;
         end
-    elseif( evtName == "showVarsWOValues") then
+    elseif( event.element.name == "showVarsWOValues") then
         if (event.element.state == true) then
             LyDevGUI.options.showVarsWOValues = true;
         else
